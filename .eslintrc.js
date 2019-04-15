c341609fddfba8ebee5a58acbfd95ff310260be8
Wing Yeung Leung <kelvinlwy2002@yahoo.com.hk>
2019-04-15 19:58:49 +1000

    Add lint config

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": "airbnb",  // Use Airbnb JS style guide
+  "parserOptions": {
+    "ecmaVersion": 6    // Support ES6 syntax
+  },
+  "env": {
+    "es6": true,        // Support new ES6 globals
+    "browser": true,    // Support browser global variables
+    "node": true        // Support Node.js global variables and Node.js scoping
+  }
+};
